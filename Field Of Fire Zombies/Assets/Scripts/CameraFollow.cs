23bbd20757b1c7a9bb8e2d6b68d874202bea4bc4
jokobro <jochemstarwarsclonewars@gmail.com>
2025-03-05 12:09:05 +0100

    camerafollow added

---
+++ Field Of Fire Zombies/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform cameraPosition;
+
+    private void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
--- Field Of Fire Zombies/Assets/Scripts/PlayerController.cs
+++ Field Of Fire Zombies/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("References")]
+    [SerializeField] private Transform cameraHolder;
     [SerializeField] private Transform orientation;
+    
+    
     CharacterController characterController;
  
 
@@ -15,17 +19,42 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 moveDirection;
     private Vector2 inputMovement;
+
+
     
+    [Header("Look Settings")]
+    [SerializeField] private float sensX = 10f;
+    [SerializeField] private float sensY = 10f;
+
+    private float yRotation;
+    private float xRotation;
 
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
     {
         HandleMovement();
+
+
+
+        float mouseX = Input.GetAxisRaw("Mouse X") * 0.1f;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * 0.1f;
+
+        yRotation += mouseX * sensX;
+        xRotation -= mouseY * sensY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
+        cameraHolder.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
     }
 
     private void HandleMovement()
